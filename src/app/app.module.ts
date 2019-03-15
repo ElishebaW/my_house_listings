0167cd20f6d03046cbecf33640a08b22ea722b7c
Elisheba Anderson <elishebaanderson@Elishebas-MacBook-Pro.local>
2019-03-15 18:50:26 -0400

    Adding house component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HouseComponent } from './house/house.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HouseComponent
   ],
   imports: [
     BrowserModule,
